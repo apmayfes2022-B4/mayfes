2e6f94d4cafa34c72c9bfa35df278fb7a50f5da3
西川直輝 <nishikawanaoki@MacBook-Pro.local>
2022-03-31 17:24:31 +0900

    Implemented communication between RaspberryPi and Kinect.

--- communication/client.py
+++ communication/client.py
@@ -1,20 +1,34 @@
+"""
+Kinectに積むコード．RaspberryPiにデータを送信する．
+"""
 import socket
 
-HOST_NAME = "127.0.0.1" # 127.0.0.1 は自分を表す # raspberrypiのipアドレスを調べて入れる必要あり
-# HOST_NAME = "157.82.203.53"
+# HOST_NAME = "127.0.0.1" # 127.0.0.1 は自分を表す # raspberrypiのipアドレスを調べて入れる必要あり
+HOST_NAME = "157.82.203.53" # raspberrypiのipアドレスを調べて入れる必要あり
 PORT = 8080
 
-# ipv4を使うので、AF_INET
-# tcp/ip通信を使いたいので、SOCK_STREAM
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def client_make_connection():
+    # ipv4を使うので、AF_INET
+    # tcp/ip通信を使いたいので、SOCK_STREAM
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    # サーバーと接続
+    sock.connect((HOST_NAME,PORT))
+    return sock
 
-# サーバーと接続
-sock.connect((HOST_NAME,PORT))
+def client_send_massage(sock, data):
+    # データ送信
+    sock.send(data.encode())
+    # サーバーからデータ受信
+    rcv_data = sock.recv(1024)
+    rcv_data = rcv_data.decode('utf-8')
+    # 正しく送信できていない場合は警告
+    if rcv_data != 'OK':
+        print("[Warning] Data was not sent correctly. ")
 
-# データ送信
-sock.send(b"Hello, TCP")
-
-# サーバーからデータ受信
-rcv_data = sock.recv(1024)
-print("receive data : {}".format(rcv_data))
-sock.close()
+if __name__ == "__main__":
+    sock = client_make_connection()
+    client_send_massage(sock, "owaneeee\n")
+    client_send_massage(sock, "abe_shinzo\n")
+    client_send_massage(sock, "kishida_humio\n")
+    client_send_massage(sock, "Yugin_Song\n")
+    sock.close()# 通信終了．これ書いとかないと通信終了しなくてだるかったりするので注意．
--- communication/server.py
+++ communication/server.py
@@ -1,39 +1,52 @@
+"""
+RaspberryPiに積むコード．Kinectからデータを受け取る．
+"""
 import socket
 
-# print(socket.gethostbyname(socket.gethostname()))
+# print(socket.gethostbyname(socket.gethostname())) # 
 
-HOST_NAME = "127.0.0.1" # 127.0.0.1 は自分を表す # raspberrypiのipアドレスを調べて入れる必要あり
-# HOST_NAME = "157.82.203.53"
+# HOST_NAME = "127.0.0.1" # 127.0.0.1 は自分を表す # raspberrypiのipアドレスを調べて入れる必要あり
+HOST_NAME = "157.82.203.53" # raspberrypiのipアドレスを調べて入れる必要あり
 PORT = 8080 # 自分で設定するもの．8080はよく使う．(well-known port)
 
-# ipv4を使うので、AF_INET
-# tcp/ip通信を使いたいので、SOCK_STREAM
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def server_make_connection():
+    # ipv4を使うので、AF_INET
+    # tcp/ip通信を使いたいので、SOCK_STREAM
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-# localhostとlocal portを指定
-sock.bind((HOST_NAME,PORT))
+    # localhostとlocal portを指定
+    sock.bind((HOST_NAME,PORT))
 
-# server動作開始
-sock.listen(1)
+    # server動作開始
+    sock.listen(1)
 
-# 接続を許可して、待つ
-client,remote_addr = sock.accept()
-print("accepted remote. remote_addr {}.".format(remote_addr))
-while True:
+    # 接続を許可して、待つ
+    client,remote_addr = sock.accept()
+    print("accepted remote. remote_addr {}.".format(remote_addr))
+
+    return sock, client
+
+def server_receive_massage(sock, client):
     # 接続されたら、データが送られてくるまで待つ
     rcv_data = client.recv(1024)
     # 接続が切られたら、終了
     if not rcv_data:
-        print("receive data don't exist")
-        break
+        print("close client communication")
+        server_end(client, sock)
+        return 0
     else:
-        print("receive data : {} ".format(rcv_data.decode("utf-8")))
-        # clientにOKを送信
-        # client.sendall('OK\n'.encode()) # b'OK\n'
-        # client.sendall(b'OK\n') # b'OK\n'
-        client.send(b'OK\n')# b'OK\n'
-print("close client communication")
+        print(rcv_data.decode("utf-8"))# データはここで取得できる
+        client.send(b'OK') # clientにOKを送信
+        return 1
 
-# clientとserverのsocketを閉じる
-client.close()
-sock.close()
+def server_end(client, sock):
+    # clientとserverのsocketを閉じる
+    client.close()
+    sock.close()
+
+if __name__ == "__main__":
+    sock, client = server_make_connection()
+    while True:
+        state = server_receive_massage(sock, client)
+        if state == 0:
+            break
